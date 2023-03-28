16cbea04571953c9c3ba94cf01ffc389729aa204
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-28 18:01:10 -0300

    adicionando título dinâmico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head"
+
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>Tasks - Organize suas tarefas</title>
+    </Head>
      <h1>Olá, mundo!</h1>
     </>
   )
