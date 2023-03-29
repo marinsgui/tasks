04eac201592e6f5108f77ecea40b9ad0136e75f4
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-29 10:50:51 -0300

    header inicial

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+      <header className="h-20 bg-slate-800">
+          <div className="max-w-6xl h-20 mx-auto flex items-center">
+              <Link href='/'>
+                  <img src="/images/tasks-logo.svg" alt="Tasks logo" className="cursor-pointer max-w-sm max-h-16" />
+              </Link>
+              <nav className="ml-20 h-20 flex items-center gap-20">
+                  <Link href='/' className="px-2 text-white border-l-2 border-orange-500">
+                      <p>Home</p>
+                  </Link>
+                  <Link href='/tasks' className="px-2 text-white border-l-2 border-orange-500">
+                      <p>Minhas tasks</p>
+                  </Link>
+              </nav>
+              <button className="ml-auto">
+                  Entrar com github
+              </button>
+          </div>
+      </header>
+  )
+}
