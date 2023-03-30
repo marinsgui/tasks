477486842be966971cd2a776542d87d5613c1cd6
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-30 12:50:08 -0300

    imagem do usuário logado

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -10,8 +10,8 @@ export default function SignInButton() {
     <button
         onClick={() => signOut()} 
         className="ml-auto h-10 rounded-3xl flex items-center justify-center gap-3 px-6 font-bold text-white hover:brightness-90 duration-200">
-            <img src="https://i.uai.com.br/X6ZjWtqD5I0elFDfdW70lU9izzo=/750x0/imgsapp2.uai.com.br/app/noticia_133890394703/2022/12/11/310968/naruto-e-um-dos-animes-mais-populares-do-mundo_1_45898.jpg" alt="Foto do usuário"  className='w-9 h-9 rounded-full'/>
-            Olá, Guilherme!
+            <img src={session.user?.image ?? ''} alt="Foto do usuário"  className='w-9 h-9 rounded-full'/>
+            Olá, {session.user?.name}!
             <FiX size={25} color='#737380' />
     </button>
   ) : (
