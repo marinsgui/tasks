16364ed9ea1d07a0754fbcce9b338c8c04dbbb77
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-30 12:43:50 -0300

    autenticação

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,12 +1,14 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 export default function SignInButton() {
-    const session = true
+  const { data: session } = useSession()
 
   return session ? (
     <button
-        onClick={() => {}} 
+        onClick={() => signOut()} 
         className="ml-auto h-10 rounded-3xl flex items-center justify-center gap-3 px-6 font-bold text-white hover:brightness-90 duration-200">
             <img src="https://i.uai.com.br/X6ZjWtqD5I0elFDfdW70lU9izzo=/750x0/imgsapp2.uai.com.br/app/noticia_133890394703/2022/12/11/310968/naruto-e-um-dos-animes-mais-populares-do-mundo_1_45898.jpg" alt="Foto do usuário"  className='w-9 h-9 rounded-full'/>
             Olá, Guilherme!
@@ -14,7 +16,7 @@ export default function SignInButton() {
     </button>
   ) : (
     <button
-        onClick={() => {}} 
+        onClick={() => signIn('github')} 
         className="ml-auto h-10 rounded-3xl flex items-center justify-center gap-3 px-6 font-bold text-white hover:brightness-90 duration-200">
             <FaGithub size={25} color='rgb(249 115 22)' />
             Entrar com github
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,15 @@ import '@/styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
+import { SessionProvider } from 'next-auth/react'
+
 import Header from '@/components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
